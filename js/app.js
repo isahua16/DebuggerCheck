62272a691b817e2b464235fbbe089fc31ced1f2f
isahua16 <isaelhuard@gmail.com>
2023-03-16 17:20:29 -0600

    Tested the debugger with a a simple conditional statement

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let username = "isael";
+let user_age = 30;
+let is_member = true;
+
+if (username === "admin" || (user_age >= 30 && is_member == true) ) {
+    console.log("This is a special greeting!");
+} else {
+    console.log("This is your standard greeting...");
+}
