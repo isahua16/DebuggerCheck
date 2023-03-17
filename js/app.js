df2e5c54b4c1716665ca4784a2ad867ceffa4c00
isahua16 <isaelhuard@gmail.com>
2023-03-16 18:37:39 -0600

    added a string concatenation

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ let username = "isael";
 let user_age = 30;
 let is_member = true;
 
-if (username === "admin" || (user_age >= 30 && is_member == true) ) {
+if (username === "admin" || (user_age >= 30 && is_member === true) ) {
     console.log("This is a special greeting!");
 } else {
     console.log("This is your standard greeting...");
 }
+
+console.log(`Hello! ${username}`);
+console.log(`You are: ${user_age} years old`);
