354bd0cd6c0aeb65458c9f9a938b3a799442647e
isahua16 <isaelhuard@gmail.com>
2023-03-16 19:12:30 -0600

    declaring arrays

--- js/app.js
+++ js/app.js
@@ -10,3 +10,13 @@ if (username === "admin" || (user_age >= 30 && is_member === true) ) {
 
 console.log(`Hello! ${username}`);
 console.log(`You are: ${user_age} years old`);
+
+let max_volume = 10.2;
+let current_volume = 3.8;
+
+console.log(max_volume / current_volume);
+console.log((max_volume / current_volume) * 100);
+
+let usernames = [`hello`, `bonjour`, `hola`, `ciao`, `hallo`];
+
+let user_ages = [10, 12, 24, 45, 67];
