afb1252b4ef6dcaf190bdc2592198fefe57f03bf
isahua16 <isaelhuard@gmail.com>
2023-03-16 19:29:57 -0600

    playing with array indexes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-let username = "isael";
-let user_age = 30;
+let username = "Isael";
+let user_age = 29;
 let is_member = true;
 
 if (username === "admin" || (user_age >= 30 && is_member === true) ) {
@@ -8,7 +8,7 @@ if (username === "admin" || (user_age >= 30 && is_member === true) ) {
     console.log("This is your standard greeting...");
 }
 
-console.log(`Hello! ${username}`);
+console.log(`Hello, ${username}!`);
 console.log(`You are: ${user_age} years old`);
 
 let max_volume = 10.2;
@@ -20,3 +20,7 @@ console.log((max_volume / current_volume) * 100);
 let usernames = [`hello`, `bonjour`, `hola`, `ciao`, `hallo`];
 
 let user_ages = [10, 12, 24, 45, 67];
+
+let first_user_name = usernames[0];
+let last_user_name = usernames[4];
+let middle_user_age = user_ages[2];
